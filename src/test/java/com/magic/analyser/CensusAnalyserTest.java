7129e5a945cc6c07326b751b588ec9a29379939e
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-06-19 16:34:42 +0530

    State Census Csv File If Incorrect Returns Custom Exception

--- src/test/java/com/magic/analyser/CensusAnalyserTest.java
+++ src/test/java/com/magic/analyser/CensusAnalyserTest.java
@@ -2,9 +2,11 @@ package com.magic.analyser;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
     private static final String INDIAN_CENSUS_CSV_FILE_PATH ="D:\\Indian State Census Analyzer\\src\\test\\resources\\StateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "D:\\Indian State Census Analyzer\\src\\main\\resources\\StateCensusData.csv";
     @Test
     public void givenIndianCensusCSC_WhenChecked_ShouldReturnCorrectRecords() {
         StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
@@ -14,5 +16,16 @@ public class CensusAnalyserTest {
         } catch (CensusAnalyserException e) {
         }
         Assertions.assertEquals(29, count);
+    }
+    @Test
+    public void givenIndianCensusCSV_WhenWrongFile_ShouldThrowException() {
+        try {
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadCensus(WRONG_CSV_FILE_PATH);
+        } catch(CensusAnalyserException e) {
+            Assertions.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
     }
 }
